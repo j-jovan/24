b36b18eb12f3fd21960ca9c22264e8924e5da117
J-Jovan <abc.j.jovan@gmail.com>
2019-04-29 19:29:23 +0200

    Find 4 random numbers

--- 24.py
+++ 24.py
@@ -3,3 +3,18 @@
 # Goal of the game is to make the number 24 using all provided digits
 # Q quits the game, N gives new numbers
 # 'En' selects English version, 'Sr' selects Serbian version of the game
+
+import random
+
+
+def choose4(): # Return 4 random digits
+    return [str(random.randint(1,9)) for i in range(4)]
+
+def numbers(numbers1):
+	print("Numbers are "+ "".join(numbers1))
+
+def main():
+	numbers1 = choose4()
+	numbers(numbers1)
+
+if __name__ == '__main__': main() # Set main() as start funct
