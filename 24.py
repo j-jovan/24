b49cbcee5fd5a0e639dd5ad962c76d73738a124f
J-Jovan <abc.j.jovan@gmail.com>
2019-04-29 18:39:02 +0200

    Add information about the game

--- 24.py
+++ 24.py
@@ -1,1 +1,5 @@
 # 'Make 24' game
+# Inspired by the code I saw on some website
+# Goal of the game is to make the number 24 using all provided digits
+# Q quits the game, N gives new numbers
+# 'En' selects English version, 'Sr' selects Serbian version of the game
