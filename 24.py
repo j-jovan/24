b11270591b689363c1f6e2e1146e666a0554094c
J-Jovan <abc.j.jovan@gmail.com>
2019-04-30 21:29:10 +0200

    Add check function

--- 24.py
+++ 24.py
@@ -1,20 +1,37 @@
 # 'Make 24' game
 # Inspired by the code I saw on some website
-# Goal of the game is to make the number 24 using all provided digits
+# Goal of the game is to make the number 24 using all provided number
 # Q quits the game, N gives new numbers
 # 'En' selects English version, 'Sr' selects Serbian version of the game
 
-import random
+from __future__ import division, print_function
+import random, ast, re
+import sys
 
-
-def choose4(): # Return 4 random digits
+def choose4(): # Return 4 random number
     return [str(random.randint(1,9)) for i in range(4)]
 
-def numbers(numbers1):
-	print("Numbers are "+ "".join(numbers1))
+def numbers(number):
+	print("Numbers are "+ "".join(number))
+
+def check(solution, number):
+    allowed = set('() +-*/\t'+''.join(number))
+    ok = all(ch in allowed for ch in solution) and \
+         all(number.count(dig) == solution.count(dig) for dig in set(number)) \
+         and not re.search('\d\d', solution)
+    if ok:
+        try:
+            ast.parse(solution)
+        except:
+            ok = False
+    return ok
 
 def main():
-	numbers1 = choose4()
-	numbers(numbers1)
+	number = choose4()
+	numbers(number)
+	solution = ''
+	provera = solution = False
+	
+	#solution = input(%i)
 
 if __name__ == '__main__': main() # Set main() as start funct
